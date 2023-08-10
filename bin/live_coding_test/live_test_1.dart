0a6275c1d332d529d53e105eb6ad205b76f026d3
mdyasinahmed <yasinahmed7875@gmail.com>
2023-08-10 20:41:20 +0600

    add live code test 1 code file

---
+++ bin/live_coding_test/live_test_1.dart
@@ -0,0 +1,13 @@
+void main(){
+  // map for person details
+  Map<String,dynamic> personInfo={
+    'PersonID': 1001,
+    'Name': 'Yasin Ahmed',
+    'Age': 20,
+    'Salary': 100000,
+    'Gender': 'Male',
+  };
+
+  // printing salary value from map
+  print(personInfo['Salary']);
+}
