bfb6010b5590242edcd6bfbaf34c20ef49887d7e
mdyasinahmed <yasinahmed7875@gmail.com>
2023-08-19 21:40:30 +0600

    update assignment 3 code

--- bin/assignment_module-3/assignment_3.dart
+++ bin/assignment_module-3/assignment_3.dart
@@ -56,14 +56,11 @@ void main(){
   car3.drive(15000);
 
   // Printing Output
-  print("Car 1: ${car1.getBrand()} ${car1.getModel()} ${car1.getYear()} "
-      "Miles: ${car1.getMilesDriven().toInt()} Age: ${car1.getAge()}");
+  print("Car 1: ${car1.getBrand()} ${car1.getModel()} ${car1.getYear()} Miles: ${car1.getMilesDriven().toInt()} Age: ${car1.getAge()}");
 
-  print("Car 2: ${car2.getBrand()} ${car2.getModel()} ${car2.getYear()} "
-      "Miles: ${car2.getMilesDriven().toInt()} Age: ${car2.getAge()}");
+  print("Car 2: ${car2.getBrand()} ${car2.getModel()} ${car2.getYear()} Miles: ${car2.getMilesDriven().toInt()} Age: ${car2.getAge()}");
 
-  print("Car 3: ${car3.getBrand()} ${car3.getModel()} ${car3.getYear()} "
-      "Miles: ${car3.getMilesDriven().toInt()} Age: ${car3.getAge()}");
+  print("Car 3: ${car3.getBrand()} ${car3.getModel()} ${car3.getYear()} Miles: ${car3.getMilesDriven().toInt()} Age: ${car3.getAge()}");
 
   print("Total number of cars: ${Car.getNumberOfCars()}");
 }
